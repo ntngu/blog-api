d05d7c24860412f6bd220dc0cb594acb4ce8e2d2
Nhan Nguyen <nhantnguyen42@gmail.com>
2022-11-23 16:05:21 -0800

    Added models.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const routes = require("./routes");
 require("dotenv").config();
 
 mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true }).then(() => {
   const app = express();
+  app.use(express.json());
+  app.use("/api", routes);
   app.listen(process.env.PORT, () => {
     console.log(`Server listening at ${process.env.PORT}`);
   });
